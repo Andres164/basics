15dcfc7b4569bde49db4feac7d0a11b8696117ad
Andres Antonio Castro Beltran <andres435@gmail.com>
2021-10-28 17:12:30 -0600

    Make a VS project, and added a class with 2 methods.

---
+++ arrayManipulation/arrayManipulation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace arrayManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            manipulateArray.add(2, 3);
+            manipulateArray.staticAdd(2, 3);
+        }
+    }
+}
